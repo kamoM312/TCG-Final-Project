c3701c13a920d5795738d057a36c9717a37fc0cf
kamo312 <kamogelomalema1@gmail.com>
2025-10-08 16:20:07 -0400

    add post - search route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ import dotenv from 'dotenv';
 const PORT = 3000;
 const app = express();
 
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 dotenv.config();
 
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
@@ -19,6 +23,25 @@ app.get("/", async (req, res) => {
   console.log(response.text);
 })
 
+// get word of the day 
+app.get("/random", async (req, res) => {
+    const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Provide a completely random english word with a definition, pronunciation, and usage example. Turn this into a json response, object, attributes: definition, pronunciation, example",
+  });
+  console.log(response.text);
+})
+
+// search for word
+app.post("/search", async (req, res) => {
+    const word = req.body.word;
+    console.log(`Search for: ${word}`)
+    const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: `Provide a definition, pronunciation, and usage example for the word '${word}'. Turn this into a json response, object, attributes: definition, pronunciation, example`,
+  });
+  console.log(response.text);
+})
 
 
 app.listen(PORT, () => {
